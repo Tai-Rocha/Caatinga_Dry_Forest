1a0c73235ed6f14b4fd3cd8133e08801aa8d0d04
Tai-Rocha <taina013@gmail.com>
2020-07-22 13:41:39 -0300

    update

--- script/Diversity index/beta_pair_funtion.R
+++ script/Diversity index/beta_pair_funtion.R
@@ -21,6 +21,20 @@ matrix_similaridade_caatinga_ <- read.csv("./data/tables/Diversity_Input_betapar
 
 jaccard_caatinga <- beta.pair(matrix_similaridade_caatinga_[,-1], index.family = "jaccard")
 
+## See outputs
+
+jaccard_caatinga[["beta.jtu"]]
+jaccard_caatinga[["beta.jne"]]
+jaccard_caatinga[["beta.jac"]]
+
+## Conversion outputs into a matrix (to save easily)
+
+beta.jtu <- as.matrix(jaccard_caatinga$beta.jtu)
+
+beta.jne <- as.matrix(jaccard_caatinga$beta.jne)
+
+beta.jac <- as.matrix(jaccard_caatinga$beta.jac)
+
 ## Sorense
 sorense_caatinga <- beta.pair(matrix_similaridade_caatinga_[,-1], index.family = "sorensen")
 
@@ -31,10 +45,10 @@ sorense_caatinga <- beta.pair(matrix_similaridade_caatinga_[,-1], index.family =
 
 ## Jaccard
 
-write.csv(jaccard_caatinga[["beta.jtu"]], "./results/Results_New/Diversity_indices/beta.pair_Jaccard_Sorense/Jaccard_beta.jtu.txt", dec = ".")
+write.csv(beta.jtu, "./results/Results_New/Diversity_indices/beta_pair_Jaccard_Sorense/Jaccard_beta_jtu.csv", sep=",", dec = ".")
 
-write.table(jaccard_caatinga[["beta.jne"]], "./results/Results_New/Diversity_indices/beta.pair_Jaccard_Sorense/Jaccard_beta.jne.txt", dec = ".")
+write.csv(beta.jne, "./results/Results_New/Diversity_indices/beta_pair_Jaccard_Sorense/Jaccard_beta_jne.csv",sep = ",", dec = ".")
 
-write.table(jaccard_caatinga[["beta.jac"]], "./results/Results_New/Diversity_indices/beta.pair_Jaccard_Sorense/Jaccard_beta.jac.txt", dec = ".")
+write.csv(beta.jac, "./results/Results_New/Diversity_indices/beta_pair_Jaccard_Sorense/Jaccard_beta_jac.csv", sep=",", dec = ".")
 
 ## Sorense
