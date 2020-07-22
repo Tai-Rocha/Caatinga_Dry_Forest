ae0ed0209efd81c61e0b52f58277dd25a9109c99
Tai-Rocha <taina013@gmail.com>
2020-07-22 13:45:15 -0300

    update

--- script/Diversity index/beta_core_function.R
+++ script/Diversity index/beta_core_function.R
@@ -19,20 +19,20 @@ matrix_similaridade_caatinga <- matrix_similaridade_caatinga_[ ,-1]
 caatinga_simi <- betapart.core(matrix_similaridade_caatinga)
 
 ####### Write Results
-write.table(caatinga_simi[["sumSi"]], "./results/Results_New/Diversity_indices/betapart.core/beta.core_sumSI", dec = ".")
+write.table(caatinga_simi[["sumSi"]], "./results/Results_New/Diversity_indices/betapart_core/beta.core_sumSI", dec = ".")
 
-write.table(caatinga_simi[["St"]], "./results/Results_New/Diversity_indices/betapart.core/beta.core_St.txt", dec = ".")
+write.table(caatinga_simi[["St"]], "./results/Results_New/Diversity_indices/betapart_core/beta.core_St.txt", dec = ".")
 
-write.table(caatinga_simi[["a"]], "./results/Results_New/Diversity_indices/betapart.core/beta.core_a.txt", dec = ".")
+write.table(caatinga_simi[["a"]], "./results/Results_New/Diversity_indices/betapart_core/beta.core_a.txt", dec = ".")
 
-write.table(caatinga_simi[["shared"]], "./results/Results_New/Diversity_indices/betapart.core/beta.core_shared.txt", dec = ".")
+write.table(caatinga_simi[["shared"]], "./results/Results_New/Diversity_indices/betapart_core/beta.core_shared.txt", dec = ".")
 
-write.table(caatinga_simi[["not.shared"]], "./results/Results_New/Diversity_indices/betapart.core/beta.core_notShared.txt", dec = ".")
+write.table(caatinga_simi[["not.shared"]], "./results/Results_New/Diversity_indices/betapart_core/beta.core_notShared.txt", dec = ".")
 
-write.table(caatinga_simi[["sum.not.shared"]], "./results/Results_New/Diversity_indices/betapart.core/beta.core_sum_notShared.txt", dec = ".")
+write.table(caatinga_simi[["sum.not.shared"]], "./results/Results_New/Diversity_indices/betapart_core/beta.core_sum_notShared.txt", dec = ".")
 
-write.table(caatinga_simi[["max.not.shared"]], "./results/Results_New/Diversity_indices/betapart.core/beta.core_maxNotShared.txt", dec = ".")
+write.table(caatinga_simi[["max.not.shared"]], "./results/Results_New/Diversity_indices/betapart_core/beta.core_maxNotShared.txt", dec = ".")
 
-write.table(caatinga_simi[["min.not.shared"]], "./results/Results_New/Diversity_indices/betapart.core/beta.core_minNotShared.txt", dec = ".")
+write.table(caatinga_simi[["min.not.shared"]], "./results/Results_New/Diversity_indices/betapart_core/beta.core_minNotShared.txt", dec = ".")
 
 
