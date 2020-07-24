1bbcab02ec1d57c50746de9267508cac016016b1
Tai-Rocha <taina013@gmail.com>
2020-07-24 13:01:49 -0300

    in progress

---
+++ script/Testes/ManteL_NMDS.R
@@ -0,0 +1,10 @@
+########################################
+## Dissimilaridade Mantel NMDS
+### Author: Tainá Rocha
+########################################
+
+## Read and Load  data 
+
+caatinga.sites.sps <- read.csv("./data/tables/Mantel and NMDS/caatinga_sites_sps.csv", sep= ",", dec = ".")
+
+caatinga_xy <-read.csv("/data/tables/Mantel and NMDS/caatinga_xy.csv", sep = ",", dec = ".")
