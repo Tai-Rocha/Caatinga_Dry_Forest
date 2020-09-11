697846ca4650efb4eba2a51e4fef9cce80537701
Tai-Rocha <taina013@gmail.com>
2020-09-11 14:08:30 -0300

    add script analysis

---
+++ script/GDM/gdm_runs.R
@@ -0,0 +1,22 @@
+#############################
+## GDM RUN
+#############################
+
+## Library
+
+library(gdm)
+
+
+## Read inputs (species_site table and enviromental table)
+sppdata <- read.csv("./data/tables/New/GDM_INPUT/sppdata.csv")
+
+envtab <- read.csv("./data/tables/New/GDM_INPUT/envtab.csv")
+
+## site xy spp list, table-table
+exFormat2a <- formatsitepair(sppdata, 2, XColumn="Long", YColumn="Lat", sppColumn="species", siteColumn="site", predData=envtab)
+
+gdm_run <- gdm(exFormat2a, geo=FALSE, splines=NULL, knots=NULL)
+
+plot(gdm_run)
+
+sta <-str(gdm_run)
