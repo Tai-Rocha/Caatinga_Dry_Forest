09d244a37a23346176716769445292d706905296
Tai-Rocha <taina013@gmail.com>
2020-12-18 14:22:41 -0300

    updated

--- script/PCoA/PCoA.R
+++ script/PCoA/PCoA.R
@@ -5,8 +5,8 @@
 
 #Library
 
-library(ape)
 library(tidyverse)
+library(vegan)
 
 
 # Vegan
@@ -49,23 +49,86 @@ abline(v=0, lty=3)
 #text(spe.wa, rownames(spe.wa), cex=0.7, col="red")
 
 
+####### Explanied percentage
+
+# Extract PCoA coordinates
+Coordinates <- data.frame(
+  Sample = row.names(pcoa_floristic_caatinga$points),
+  Dim1 = pcoa_floristic_caatinga$points[,1],
+  Dim2 = pcoa_floristic_caatinga$points[,2],
+  row.names = NULL
+) %>%
+  as.tbl()
+
+# Calculate variance explained
+# Method is from http://r-sig-ecology.471788.n2.nabble.com/\
+# Variability-explanations-for-the-PCO-axes-as-in-Anderson-and-Willis-2003-\
+# td6429547.html
+Eigenvalues <- eigenvals(pcoa_floristic_caatinga) 
+Variance <- Eigenvalues / sum(Eigenvalues) 
+Variance1 <- 100 * signif(Variance[1], 2)
+Variance2 <- 100 * signif(Variance[2], 2)
+
+# Return
+Result <- list(
+  Coordinates = Coordinates,
+  Variance1 = Variance1,
+  Variance2 = Variance2
+)
+return(Result)
+
+
 ############ Climatic data
 
 #Read data 
 
-climatic_caatinga <- read.csv("./data/tables/New/PcoA/PCoA_long.csv")
+climatic_caatinga <- read.csv("./data/tables/New/PcoA/PCoA_envs.csv")
+
+## To get the names of sites
+rownames(climatic_caatinga) <- climatic_caatinga[,1]
+climatic_caatinga <- climatic_caatinga[,-1]
+climatic_caatinga
 
 ###  MAtrix Distance based on climatic data in points
 
-Euclidian_Climatic <- dist(climatic_caatinga[3:25], method = "euclidean")
+Euclidian_Climatic <- dist(climatic_caatinga, method = "euclidean")
 
-## PCoA
-
-#Ape
-climatic_caatina_pcoa <- pcoa(Euclidian_Climatic, correction="none", rn=climatic_caatinga$sitios)
 
 # Vegan
 eucl_clim <-cmdscale(Euclidian_Climatic,k=2,eig=TRUE)
 eucl_clim
 
-biplot(climatic_caatina_pcoa,rn=climatic_caatinga$sitios,pch=25, main = "PCoA of euclidean distance", xlab="Dim1", ylab="Dim2")
+pcoa_climatic <-ordiplot(scores(eucl_clim, choices = c(1,2)), type = "t", main= "PCoA of climatic matrix")
+abline(h=0, lty=3)
+abline(v=0, lty=3)
+
+####### Explanied percentage
+
+# Extract PCoA coordinates
+Coordinates <- data.frame(
+  Sample = row.names(eucl_clim$points),
+  Dim1 = eucl_clim$points[,1],
+  Dim2 = eucl_clim$points[,2],
+  row.names = NULL
+) %>%
+  as.tbl()
+
+# Calculate variance explained
+# Method is from http://r-sig-ecology.471788.n2.nabble.com/\
+# Variability-explanations-for-the-PCO-axes-as-in-Anderson-and-Willis-2003-\
+# td6429547.html
+Eigenvalues <- eigenvals(eucl_clim) 
+Variance <- Eigenvalues / sum(Eigenvalues) 
+Variance1 <- 100 * signif(Variance[1], 2)
+Variance2 <- 100 * signif(Variance[2], 2)
+
+# Return
+Result <- list(
+  Coordinates = Coordinates,
+  Variance1 = Variance1,
+  Variance2 = Variance2
+)
+return(Result)
+
+
+
