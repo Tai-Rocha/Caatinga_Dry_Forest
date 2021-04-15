fbdb49b6b40be382c413aa583f8be66aa0ff07c9
Tai-Rocha <taina013@gmail.com>
2021-04-15 17:02:29 +0000

    updated

--- script/get_data/05_get_records.R
+++ script/get_data/05_get_records.R
@@ -1,9 +1,10 @@
 # Download species records
 # get from each database, then merge final results
 
-#library(Rocc)
+library(Rocc)
 library(dplyr)
 library(tidyverse)
+library(erer)
 
 # reading final species list
 spp <- read.csv("./results/04_taxon_data_flora_check.csv")
@@ -27,11 +28,34 @@ for (i in 1:length(spp2)) {
                                     Scope = "plants")
 }
 
+############## Bind Rows 
+
+lista_2 <- bind_rows(records_list)
+
+############### Write table
+
+write.csv(lista_2, "final_2.csv", row.names = FALSE)
+
+#j <- length(records_list)
+
+#for (j in 1:length(records_list)) {
+#  write.csv(records_list[j], file = paste0("loop_", ".csv"))
+#}
+
+
+#for (j in 1:length(records_list)) {
+#     write.csv(records_list[j], file = paste0("loop_", j, ".csv"))
+#
+
+#for (j in 1:length(records_list)) {
+#write.list(records_list[j], file= "spps_records_final.csv", row.names = T)
+#}
+
+##################################################################################################
 
 #records <- rspeciesLink(dir = "./results/spp_records/",
 #                        filename = "records_specieslink",
 #                        species = spp2[1:19],
 #                        Scope = "plants")
-
 #records_1 <- purrr::map(spp2, rspeciesLink(dir = "./results/spp_records/",
 #                                                       filename = "records_specieslink"))
