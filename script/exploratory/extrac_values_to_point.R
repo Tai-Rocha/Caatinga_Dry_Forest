5c86f9fa80c60fc29ac4dd9a8337bb9a89e1785d
Tai-Rocha <taina013@gmail.com>
2021-05-18 18:45:38 -0300

    updating...

--- script/exploratory/extrac_values_to_point.R
+++ script/exploratory/extrac_values_to_point.R
@@ -1,31 +1,37 @@
-######################
-# Estudando os pontos
-#Author: Tainá Rocha
-#####################
+############################################
+# Enviromental values of species points 
+# Author: Tainá Rocha
+###########################################
 
-## Library
+##################
+# Library
 
 library(raster)
+
 ##################
 
 ### Load Points
 
-caatinga_pontos <- read.csv("./data/tables/New/vars_in_cood/vals_in_coord_input.csv", sep = ",", dec = ".")
+points_cc <- read.csv("./data/tables/GDM_INPUT/SPP_DATA_ID.csv", sep = ",", dec = ".")
 
 # remover registros duplicados
-dups.all <- duplicated(caatinga_pontos[, c('LAT', 'LONG')])
-caatinga_pontos_final <- caatinga_pontos[!dups.all, ]
+#dups.all <- duplicated(caatinga_pontos[, c('LAT', 'LONG')])
+#caatinga_pontos_final <- caatinga_pontos[!dups.all, ]
 
 
 ##### Extract values from point (require a file in format long lat respectively to use extract funtion)
 
-list_envs <- list.files ("./data/Envs_caatinga/", full.names = T, pattern = ".tif")
+list_envs <- list.files ("./data/Envs_GDM/5km/", full.names = T, pattern = ".tif")
 
 envs <- stack(list_envs)
 
 # Extract values from point
 
-values_in_coord <- extract(envs, caatinga_pontos[3:4])
+#values_in_coord <- extract(envs, points_cc[4:5])
+#write.csv(values_in_coord, "./data/tables/GDM_INPUT/env_values_in_coord.csv", sep = ",")
 
-write.table(values_in_coord, "./data/tables/New/vars_in_cood/caatinga_values_in_coord.txt")
+values_in_coord_bi <- extract(envs, points_cc[4:5], method= 'bilinear')
+
+write.csv(values_in_coord_bi, "./data/tables/GDM_INPUT/env_values_in_coord_bilinear.csv", sep = ",")
+
 
