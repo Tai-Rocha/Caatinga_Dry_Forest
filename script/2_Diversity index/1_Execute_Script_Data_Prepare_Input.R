fc047140c21621f5b7bc39aee5540927e4b05ce5
tai-rocha <taina@ModleR2.pilf5hepncculcfcu3aypoxzxb.xx.internal.cloudapp.net>
2021-06-24 22:35:16 +0000

    updating...

---
+++ script/2_Diversity index/1_Execute_Script_Data_Prepare_Input.R
@@ -0,0 +1,15 @@
+
+########################################################################################################################
+### This command will run the script called “NOT_OPEN_betapart_data_prepare.R”
+### If you try to run “NOT_OPEN_betapart_data_prepare.R” probably your section will crash, once this is large script
+### R version 4.1
+### 24 June 2021
+### Author: Tainá Rocha
+########################################################################################################################
+
+
+source("./script/2_Diversity index/NOT_OPEN_betapart_data_prepare.R", echo = TRUE)
+
+
+
+
